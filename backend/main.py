d3aed4986c2e9eb6dfa985eb4a351ed1694fdb5a
patrick.carvalhosouz@gmail.com <patrick.carvalhosouz@gmail.com>
2026-05-29 21:40:28 -0300

    allow_credentials=False

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Para testes, "*" libera tudo
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
