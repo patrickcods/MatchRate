a9c01a9a5dc5d33d0d2a2e9338289af35ea0d46b
patrick.carvalhosouz@gmail.com <patrick.carvalhosouz@gmail.com>
2026-06-03 21:39:29 -0300

    mudando db

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./matchrate.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./matchrate.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
