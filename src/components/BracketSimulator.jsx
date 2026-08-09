a97b80487e31f9cbed57a1901b9cbe6cde948abc
patrick.carvalhosouz@gmail.com <patrick.carvalhosouz@gmail.com>
2026-08-09 19:23:41 -0300

    Comentarios no Simulador

--- src/components/BracketSimulator.jsx
+++ src/components/BracketSimulator.jsx
@@ -52,6 +52,7 @@ const BotoesCompartilhar = ({ texto, refArea }) => {
     }
   };
 
+  {'Função PRINT'}
   const tirarPrint = async () => {
     if (!refArea?.current) return;
     const canvas = await html2canvas(refArea.current, { backgroundColor: '#121212', scale: 2 });
